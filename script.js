38190751d47c60bef913a1634f2b1749b0d9f5c3
Kenneth <kennethlorenzg@gmail.com>
2023-09-27 19:52:01 -0400

    create initial script

---
+++ script.js
@@ -0,0 +1,10 @@
+var firstNumber;
+var secondNumber;
+var operator;
+
+function add() {}
+function subtract() {}
+function multiply() {}
+function divide() {}
+
+function operate(firstNumber, operator, secondNumber) {}
