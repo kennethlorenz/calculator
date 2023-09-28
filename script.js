086de76fabab7925f2cf3328c2ab479a8714e150
Kenneth <kennethlorenzg@gmail.com>
2023-09-28 17:50:02 -0400

    Add displayInput & allClear function & Operate fun
    Display input allows users to display the selected numbers and decimal.
    
    allClear function clears the input.
    Operate function allows users to enter 2 numbers and an operator.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-var firstNumber = 0;
+var firstNumber = "";
 var secondNumber;
 var operator;
 document.querySelector(".currentYear").textContent = new Date().getFullYear();
 const input = document.querySelector(".input.main");
-input.innerHTML = firstNumber;
-
-var buttons = document.querySelectorAll(".number");
+const acButton = document.querySelector(".ac");
+var buttons = document.querySelectorAll(".btn");
 
 function add(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
@@ -24,21 +23,33 @@ function operate(firstNumber, operator, secondNumber) {
   if (operator == "+") {
     return add(firstNumber, secondNumber);
   } else if (operator == "-") {
-    return firstNumber, secondNumber;
+    return subtract(firstNumber, secondNumber);
   } else if (operator == "*") {
-    return firstNumber, secondNumber;
+    return multiply(firstNumber, secondNumber);
   } else if (operator == "/") {
     return divide(firstNumber, secondNumber);
   }
 }
 
-console.log(operate(1, "+", 1));
-
 function displayInput(e) {
-  if (e.target.innerHTML !== "0") {
+  if (e.target.innerHTML == 0 && input.innerHTML.startsWith("0")) {
+    console.log("0 clicked");
+    return;
+  } else if (e.target.innerHTML == "." && input.innerHTML.startsWith("0")) {
+    firstNumber += 0 + ".";
+    input.innerHTML = firstNumber;
+  } else if (e.target.innerHTML == "." && input.innerHTML.indexOf(".") !== -1) {
+    return;
+  } else {
     firstNumber += e.target.innerHTML;
     input.innerHTML = firstNumber;
   }
-  return;
+  console.log(firstNumber);
+}
+
+function allClear() {
+  firstNumber = "0";
+  input.innerHTML = firstNumber;
 }
 buttons.forEach((button) => button.addEventListener("click", displayInput));
+acButton.addEventListener("click", allClear);
