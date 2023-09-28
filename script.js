ebe74bc7922934014396126cdb3e0733163e6935
Kenneth <kennethlorenzg@gmail.com>
2023-09-27 22:22:32 -0400

    populate functions for add, sub, multiply, divide & Operate. Add Displayinput function

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-var firstNumber;
+var firstNumber = 0;
 var secondNumber;
 var operator;
 document.querySelector(".currentYear").textContent = new Date().getFullYear();
+const input = document.querySelector(".input.main");
+input.innerHTML = firstNumber;
 
-function add() {}
-function subtract() {}
-function multiply() {}
-function divide() {}
+var buttons = document.querySelectorAll(".number");
 
-function operate(firstNumber, operator, secondNumber) {}
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+function subtract(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+function multiply(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+function divide(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
+
+function operate(firstNumber, operator, secondNumber) {
+  if (operator == "+") {
+    return add(firstNumber, secondNumber);
+  } else if (operator == "-") {
+    return firstNumber, secondNumber;
+  } else if (operator == "*") {
+    return firstNumber, secondNumber;
+  } else if (operator == "/") {
+    return divide(firstNumber, secondNumber);
+  }
+}
+
+console.log(operate(1, "+", 1));
+
+function displayInput(e) {
+  if (e.target.innerHTML !== "0") {
+    firstNumber += e.target.innerHTML;
+    input.innerHTML = firstNumber;
+  }
+  return;
+}
+buttons.forEach((button) => button.addEventListener("click", displayInput));
