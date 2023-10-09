d40effc189e8e7189525521ade2949f28995b297
Kenneth <kennethlorenzg@gmail.com>
2023-10-09 01:51:07 -0400

    Fixed 0 and Decimal Issue.
    Fixed issue where zero or decimal is not being properly displayed
    when a user clicks on an operator after clicking equal sign.

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ var buttons = document.querySelectorAll(".btn");
 var operators = document.querySelectorAll(".operator");
 var resultScreen = document.querySelector(".input.result");
 var resultScreenDisplay = "";
+var inputDisplay = "";
+input.textContent = 0;
 
 function add(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
@@ -33,9 +35,10 @@ function operate(firstNumber, operator, secondNumber) {
     return add(firstNum, secondNum);
   } else if (operator == "-") {
     return subtract(firstNum, secondNum);
-  } else if (operator == "*") {
+  } else if (operator == "x") {
     return multiply(firstNum, secondNum);
-  } else if (operator == "/") {
+  } else if (operator == "÷") {
+    console.log(divide(firstNum, secondNum));
     return divide(firstNum, secondNum);
   }
 }
@@ -46,32 +49,62 @@ function updateDisplay(firstNumber, operator, secondNumber) {
 }
 
 function displayInput(e) {
-  //
-  if (e.target.textContent == 0 && input.textContent.startsWith("0")) {
+  // Checks if the user has entered 0 after 0.
+  //Allows the user to enter zero after a decimal point
+  // 0.000000
+  if (
+    e.target.textContent == 0 &&
+    input.textContent.startsWith("0") &&
+    input.textContent.includes(".")
+  ) {
+    inputDisplay += e.target.textContent;
+    input.textContent = inputDisplay;
+    console.log("first if");
+  }
+  //allows user to enter only one 0 if the initial value is 0
+  else if (e.target.textContent == 0 && input.textContent.startsWith("0")) {
+    console.log("second if");
     return;
-  } else if (e.target.textContent == "." && input.textContent.startsWith("0")) {
-    firstNumber += 0 + ".";
-    input.textContent = firstNumber;
+  }
+  //
+  else if (e.target.textContent == "." && input.textContent.startsWith("0")) {
+    if (!input.textContent.includes(".")) {
+      inputDisplay = 0 + ".";
+      console.log("3rd if");
+      input.textContent = inputDisplay;
+      console.log(inputDisplay);
+    }
   } else if (
     e.target.textContent == "." &&
     input.textContent.indexOf(".") !== -1
   ) {
     return;
   } else if (firstNumberDuplicate.length >= 20) {
-    firstNumber += e.target.textContent;
-    firstNumberDuplicate = firstNumber;
-    input.textContent = convertToExponential(firstNumber);
-  } else if (
-    resultScreen.textContent.length >= 1 ||
-    resultScreen.textContent === "0"
-  ) {
-    secondNumber += e.target.textContent;
-    input.textContent = secondNumber;
+    inputDisplay += e.target.textContent;
+    firstNumberDuplicate = inputDisplay;
+    input.textContent = convertToExponential(inputDisplay);
+    // } else if (
+    //   resultScreen.textContent.length >= 1 ||
+    //   resultScreen.textContent === "0"
+    // ) {
+    //   secondNumber += e.target.textContent;
+    //   input.textContent = secondNumber;
   } else {
-    firstNumber += e.target.textContent;
-    firstNumberDuplicate = firstNumber;
-    input.textContent = firstNumber;
+    if (e.target.textContent == "." && inputDisplay == "") {
+      inputDisplay += 0 + ".";
+      firstNumberDuplicate = inputDisplay;
+      input.textContent = inputDisplay;
+    } else if (e.target.textContent == "0" && inputDisplay == "") {
+      input.textContent = 0;
+      console.log("test");
+      return;
+    } else {
+      inputDisplay += e.target.textContent;
+      firstNumberDuplicate = inputDisplay;
+      input.textContent = inputDisplay;
+    }
   }
+  console.log(input.textContent);
 }
 
 function convertToExponential(number) {
@@ -84,18 +117,21 @@ function allClear() {
   secondNumber = "";
   input.textContent = "0";
   resultScreen.textContent = "";
+  inputDisplay = "";
 }
 
 function clear() {
-  firstNumber = input.textContent.slice(0, -1);
+  inputDisplay = input.textContent.slice(0, -1);
   console.log(firstNumber);
-  input.textContent = firstNumber;
+  input.textContent = inputDisplay;
   if (input.textContent === "") {
     input.textContent = 0;
   }
 }
 
 function operatorClicked(e) {
+  inputDisplay = input.textContent;
+  firstNumber = inputDisplay;
   operator = e.target.textContent;
   if (input.textContent === "0") {
     resultScreenDisplay = `${0} ${e.target.textContent}`;
@@ -103,14 +139,25 @@ function operatorClicked(e) {
     resultScreenDisplay = `${firstNumber} ${operator}`;
   }
   resultScreen.textContent = resultScreenDisplay;
+  inputDisplay = "";
 }
 
 function calculate() {
-  updateDisplay(firstNumber, operator, secondNumber);
-  result = operate(firstNumber, operator, secondNumber);
-  input.textContent = result;
-  secondNumber = "";
-  firstNumber = result;
+  secondNumber = input.textContent;
+  if (secondNumber == "" && firstNumber == "") {
+    return;
+  }
+  if (!firstNumber == "" && !secondNumber == "") {
+    console.log(input.textContent);
+    secondNumber = input.textContent;
+    updateDisplay(firstNumber, operator, secondNumber);
+    result = operate(firstNumber, operator, secondNumber);
+    inputDisplay = result;
+    input.textContent = inputDisplay;
+    secondNumber = "";
+    inputDisplay = "";
+  }
+  console.log(input.textContent);
 }
 
 buttons.forEach((button) => button.addEventListener("click", displayInput));
