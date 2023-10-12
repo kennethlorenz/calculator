45b889e3e3de732cd6b7d2b66faa0f021f909ff8
Kenneth <kennethlorenzg@gmail.com>
2023-10-11 21:05:40 -0400

    refactored and added comments

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ var operator = "";
 var firstNumberDuplicate = "";
 var result = "";
 document.querySelector(".currentYear").textContent = new Date().getFullYear();
-const input = document.querySelector(".input.main");
+const mainScreen = document.querySelector(".input.main");
 const allClearButton = document.querySelector(".ac");
 const clearButton = document.querySelector(".c");
 const equalButton = document.querySelector(".equal");
 var buttons = document.querySelectorAll(".btn");
 var operators = document.querySelectorAll(".operator");
-var resultScreen = document.querySelector(".input.result");
-var resultScreenDisplay = "";
-var inputDisplay = "";
-input.textContent = 0;
+var secondScreen = document.querySelector(".input.result");
+var secondScreenDisplay = "";
+var mainScreenDisplay = "";
+mainScreen.textContent = 0;
 
 function add(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
@@ -43,68 +43,77 @@ function operate(firstNumber, operator, secondNumber) {
   }
 }
 
-function updateDisplay(firstNumber, operator, secondNumber) {
-  resultScreenDisplay = `${firstNumber} ${operator} ${secondNumber} =`;
-  resultScreen.textContent = resultScreenDisplay;
-}
-
 function displayInput(e) {
-  // Checks if the user has entered 0 after 0.
+  // Checks if the user has entered 0 after 0 and a decimal (.)
   //Allows the user to enter zero after a decimal point
   // 0.000000
   if (
     e.target.textContent == 0 &&
-    input.textContent.startsWith("0") &&
-    input.textContent.includes(".")
+    mainScreen.textContent.startsWith("0") &&
+    mainScreen.textContent.includes(".")
   ) {
-    inputDisplay += e.target.textContent;
-    input.textContent = inputDisplay;
+    mainScreenDisplay += e.target.textContent;
+    mainScreen.textContent = mainScreenDisplay;
     console.log("first if");
   }
   //allows user to enter only one 0 if the initial value is 0
-  else if (e.target.textContent == 0 && input.textContent.startsWith("0")) {
+  else if (
+    e.target.textContent == 0 &&
+    mainScreen.textContent.startsWith("0")
+  ) {
     console.log("second if");
     return;
   }
-  //
-  else if (e.target.textContent == "." && input.textContent.startsWith("0")) {
-    if (!input.textContent.includes(".")) {
-      inputDisplay = 0 + ".";
-      console.log("3rd if");
-      input.textContent = inputDisplay;
-      console.log(inputDisplay);
-    }
-  } else if (
+  //checks if the user clicks on decimal (.) and the initial value is 0
+  else if (
     e.target.textContent == "." &&
-    input.textContent.indexOf(".") !== -1
+    mainScreen.textContent.startsWith("0")
   ) {
-    return;
-  } else if (firstNumberDuplicate.length >= 20) {
-    inputDisplay += e.target.textContent;
-    firstNumberDuplicate = inputDisplay;
-    input.textContent = convertToExponential(inputDisplay);
-    // } else if (
-    //   resultScreen.textContent.length >= 1 ||
-    //   resultScreen.textContent === "0"
-    // ) {
-    //   secondNumber += e.target.textContent;
-    //   input.textContent = secondNumber;
-  } else {
-    if (e.target.textContent == "." && inputDisplay == "") {
-      inputDisplay += 0 + ".";
-      firstNumberDuplicate = inputDisplay;
-      input.textContent = inputDisplay;
-    } else if (e.target.textContent == "0" && inputDisplay == "") {
-      input.textContent = 0;
-      console.log("test");
-      return;
-    } else {
-      inputDisplay += e.target.textContent;
-      firstNumberDuplicate = inputDisplay;
-      input.textContent = inputDisplay;
+    //if input doesn't have a decimal and a user clicks on decimal
+    //add 0 as a first value followed with a decimal.
+    if (!mainScreen.textContent.includes(".")) {
+      mainScreenDisplay = 0 + ".";
+      console.log("3rd if");
+      mainScreen.textContent = mainScreenDisplay;
+      console.log(mainScreenDisplay);
     }
   }
-  console.log(input.textContent);
+  //checks if the user clicks on decimal (.) and if there's already a decimal on the input
+  //if there is we return as default
+  else if (
+    e.target.textContent == "." &&
+    mainScreen.textContent.indexOf(".") !== -1
+  ) {
+    return;
+  }
+  //convert a number to exponential if the input is greater than or equal to 20
+  else if (firstNumberDuplicate.length >= 20) {
+    mainScreenDisplay += e.target.textContent;
+    firstNumberDuplicate = mainScreenDisplay;
+    mainScreen.textContent = convertToExponential(mainScreenDisplay);
+  } else {
+    //check if the user clicks on decimal (.) and the input is empty
+    //add 0 as initial value followed by a decimal
+    if (e.target.textContent == "." && mainScreenDisplay == "") {
+      mainScreenDisplay += 0 + ".";
+      firstNumberDuplicate = mainScreenDisplay;
+      mainScreen.textContent = mainScreenDisplay;
+    }
+    //checks if the user selects 0 and the initial value is empty
+    //set the initial value of the content to 0
+    else if (e.target.textContent == "0" && mainScreenDisplay == "") {
+      mainScreen.textContent = 0;
+      console.log("test");
+      return;
+    }
+    //add the numbers into the input
+    else {
+      mainScreenDisplay += e.target.textContent;
+      firstNumberDuplicate = mainScreenDisplay;
+      mainScreen.textContent = mainScreenDisplay;
+    }
+  }
+  console.log(mainScreen.textContent);
 }
 
 function convertToExponential(number) {
@@ -115,55 +124,54 @@ function allClear() {
   firstNumber = "";
   firstNumberDuplicate = "";
   secondNumber = "";
-  input.textContent = "0";
-  resultScreen.textContent = "";
-  inputDisplay = "";
+  mainScreen.textContent = 0;
+  secondScreen.textContent = "";
+  mainScreenDisplay = "";
 }
 
 function clear() {
-  inputDisplay = input.textContent.slice(0, -1);
+  mainScreenDisplay = mainScreen.textContent.slice(0, -1);
   console.log(firstNumber);
-  input.textContent = inputDisplay;
-  if (input.textContent === "") {
-    input.textContent = 0;
+  mainScreen.textContent = mainScreenDisplay;
+  if (mainScreen.textContent === "") {
+    mainScreen.textContent = 0;
   }
 }
 
-function operatorClicked(e) {
-  inputDisplay = input.textContent;
-  firstNumber = inputDisplay;
+function updateDisplay(firstNumber, operator, secondNumber) {
+  secondScreenDisplay = `${firstNumber} ${operator} ${secondNumber} =`;
+  secondScreen.textContent = secondScreenDisplay;
+}
+
+function updateSecondScreen(e) {
+  firstNumber = mainScreen.textContent;
   operator = e.target.textContent;
-  if (input.textContent === "0") {
-    resultScreenDisplay = `${0} ${e.target.textContent}`;
-  } else {
-    resultScreenDisplay = `${firstNumber} ${operator}`;
-  }
-  resultScreen.textContent = resultScreenDisplay;
-  inputDisplay = "";
+  secondScreenDisplay = `${firstNumber} ${operator}`;
+  secondScreen.textContent = secondScreenDisplay;
+  mainScreenDisplay = "";
 }
 
 function calculate() {
-  secondNumber = input.textContent;
+  secondNumber = mainScreen.textContent;
   if (secondNumber == "" && firstNumber == "") {
     return;
   }
   if (!firstNumber == "" && !secondNumber == "") {
-    console.log(input.textContent);
-    secondNumber = input.textContent;
+    console.log(mainScreen.textContent);
+    secondNumber = mainScreen.textContent;
     updateDisplay(firstNumber, operator, secondNumber);
     result = operate(firstNumber, operator, secondNumber);
-    inputDisplay = result;
-    input.textContent = inputDisplay;
+    mainScreen.textContent = result;
     secondNumber = "";
-    inputDisplay = "";
+    mainScreenDisplay = "";
   }
-  console.log(input.textContent);
+  console.log(mainScreen.textContent);
 }
 
 buttons.forEach((button) => button.addEventListener("click", displayInput));
 allClearButton.addEventListener("click", allClear);
 operators.forEach((operator) =>
-  operator.addEventListener("click", operatorClicked)
+  operator.addEventListener("click", updateSecondScreen)
 );
 
 clearButton.addEventListener("click", clear);
